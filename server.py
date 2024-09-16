3e48248aa540a25479907708598eae20586cdce3
ansahmohammad <ansahk@gmail.com>
2024-09-16 21:10:20 +0530

    feat: added flask skeleton

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from core import Pixie
+
+app = Flask(__name__)
+pix = Pixie()
+
+@app.route('/')
+def home():
+    return render_template('pixie.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    destination = request.form['destination']
+    days = request.form['days']
+    date = request.form['date']
+    budget = request.form['budget']
+    diet = request.form['diet']
+    interests = request.form.getlist('interests')
+    comments = request.form['comments']
+    
+    prompt = pix.prompt_generator(destination, days, budget, diet, interests, comments)
+    print("Prompt: ", prompt)
+    
+    return render_template('pixie.html')
+
+
+if __name__ == '__main__':
+    app.run()
